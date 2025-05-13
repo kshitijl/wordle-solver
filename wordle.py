6c51ea1cdfd3dcb0fa52a461d1481ebe66043dc5
Kshitij Lauria <kslauria@gmail.com>
2025-05-13 15:25:26 -0500

    fix functions to be able to play for real after the precomputation optimizations

--- wordle.py
+++ wordle.py
@@ -1,11 +1,11 @@
-from collections import defaultdict
 from enum import Enum
 from math import log
 import time
 from contextlib import contextmanager
-from typing import List, Any
+from typing import List
 import numpy as np
 import h5py
+import bisect
 
 
 @contextmanager
@@ -63,6 +63,14 @@ def create_response(s: str) -> Response:
     return Response(response_packed)
 
 
+# TODO: BUG: it deals with duplicate letters incorrectly.
+#
+# predict_("combo", "macho") returns WWWNR right now.
+# But it should be WNWNR.
+#
+# In other words, if a single O is being marked as R (green), then it is
+# "taken", and cannot be used to mark the other O as W (yellow). The othe O
+# should be marked as N (grey).
 def predict_(move: str, answer: str) -> Response:
     """If the true answer to the puzzle is the word [answer], and we play the
     word [move], then what would be the game's [Response], i.e., the clues that
@@ -88,15 +96,6 @@ def predict_(move: str, answer: str) -> Response:
 
 def predict(dictionary: List[str], move: Move, answer: Answer) -> Response:
     return predict_(dictionary[move], dictionary[answer])
-
-
-def is_consistent(answer: Answer, move: Move, response: Response) -> bool:
-    """If the true answer to the puzzle is the word [answer], and we play the
-    word [move], then would the game respond with [response]?
-
-    """
-    # return predict(move, answer) == response
-    return True
 
 
 def compute_entropy(dist: np.ndarray) -> float:
@@ -130,28 +129,42 @@ def compute_entropy_dict(dist: dict[Response, int]) -> float:
 
 
 class GameState(object):
-    def __init__(self, dictionary: List[str]):
+    def __init__(self, dictionary_filename: str):
+        dictionary = [x.strip() for x in open(dictionary_filename).readlines()]
+        dictionary = list(sorted([x.lower() for x in dictionary if len(x) == 5]))
+
         self.dictionary: List[str] = dictionary
         self.possible_answers: List[Answer] = [
             Answer(x) for x in range(len(self.dictionary))
         ]
 
-    # def _eliminate(self, move: Move, response: Response):
-    #     self.possible_answers = [
-    #         x for x in self.possible_answers if is_consistent(x, move, response)
-    #     ]
+    def is_consistent(self, answer: Answer, move: Move, response: Response) -> bool:
+        """If the true answer to the puzzle is the word [answer], and we play the
+        word [move], then would the game respond with [response]?
 
-    # def eliminate(self, move, response):
-    #     """We have played the word [move] and the game responded with
-    #     [response]. Eliminate all possibilities that are inconsistent with that
-    #     response.
+        """
+        total_moves = len(self.dictionary)
+        return self.predictions[move * total_moves + answer] == response
 
-    #     """
-    #     print(f"Before: {len(self.possible_answers)} possibilities")
-    #     self._eliminate(Move(move), create_response(response))
-    #     print(f"After: {len(self.possible_answers)} possibilities")
-    #     if len(self.possible_answers) < 20:
-    #         print(self.possible_answers)
+    def _eliminate(self, move: Move, response: Response):
+        self.possible_answers = [
+            x for x in self.possible_answers if self.is_consistent(x, move, response)
+        ]
+
+    def eliminate(self, move: str, response: str):
+        """We have played the word [move] and the game responded with
+        [response]. Eliminate all possibilities that are inconsistent with that
+        response.
+
+        """
+        move_idx = bisect.bisect_left(self.dictionary, move)
+        if not (move_idx < len(self.dictionary) and self.dictionary[move_idx] == move):
+            raise ValueError("Move not found in dictionary")
+        print(f"Before: {len(self.possible_answers)} possibilities")
+        self._eliminate(Move(move_idx), create_response(response))
+        print(f"After: {len(self.possible_answers)} possibilities")
+        if len(self.possible_answers) < 20:
+            print([self.dictionary[x] for x in self.possible_answers])
 
     def compute_all_predictions(self):
         num_words = len(self.dictionary)
@@ -176,7 +189,28 @@ class GameState(object):
                     possible_move_idx * num_words + possible_answer_idx
                 ] = prediction
 
-    def best_move(self) -> Move:
+    def load_predictions(self, predictions_filename: str):
+        with h5py.File(predictions_filename, "r") as f:
+            self.predictions = f["data_array"][:]
+
+        if len(self.dictionary) * len(self.dictionary) != self.predictions.shape[0]:
+            raise ValueError("Dictionary length not consistent with loaded predictions")
+
+    def save_predictions(self, predictions_filename: str):
+        with h5py.File(predictions_filename, "w") as f:
+            dset = f.create_dataset(
+                "data_array",
+                data=self.predictions,
+                compression="gzip",
+                compression_opts=4,
+            )
+            dset.attrs["num_words"] = len(self.dictionary)
+            dset.attrs["version"] = 1
+
+    # TODO: if there are only two words left, it's better to guess one of them.
+    # Generalize this in an elegant mathematical way: we want to balance
+    # getting more information with actually winning the game.
+    def best_move(self) -> str:
         """Given the current state of the game, what's the best move?
 
         The best move is the one that gives us "most information".
@@ -204,12 +238,12 @@ class GameState(object):
 
         """
         if len(self.possible_answers) == 1:
-            return Move(list(self.possible_answers)[0])
+            return self.dictionary[list(self.possible_answers)[0]]
+
+        if not hasattr(self, "predictions"):
+            self.compute_all_predictions()
 
         total_moves = len(self.dictionary)
-
-        # response_distribution = np.zeros((total_moves, 256))
-        # response_distribution: dict[Move, dict] = defaultdict(lambda: defaultdict(int))
 
         print_every = int(total_moves / 10)
         print(
@@ -217,15 +251,18 @@ class GameState(object):
         )
 
         entropy: dict[Move, float] = {}
-        # response_distribution = np.zeros(256, dtype=int)
 
         for possible_move_idx in range(len(self.dictionary)):
             if possible_move_idx % print_every == 0:
                 print(f"Evaluated {possible_move_idx} out of {total_moves} moves")
 
             move = Move(possible_move_idx)
-            # response_distribution: dict = defaultdict(int)
-            # response_distribution.fill(0)
+
+            # This was faster than using a defaultdict, which was itself faster
+            # than using a numpy array of size 256. The slow step in all these
+            # is incrementing the counter at position [prediction]. I don't
+            # know why the numpy array was slowest; I tried a couple of data
+            # types.
             response_distribution = [0] * 256
             for answer in self.possible_answers:
                 prediction: Response = self.predictions[
@@ -237,12 +274,13 @@ class GameState(object):
         entropy_list = list(reversed(sorted(entropy.items(), key=lambda y: y[1])))
         for top10_item in entropy_list[:10]:
             print((self.dictionary[top10_item[0]], top10_item[1]))
-        return entropy_list[0][0]
+        return self.dictionary[entropy_list[0][0]]
 
 
 if __name__ == "__main__":
     compute_predictions = False
     common_words_only = False
+    compute_best_first_move = False
 
     if common_words_only:
         dictionary_file, predictions_file = (
@@ -252,23 +290,15 @@ if __name__ == "__main__":
     else:
         dictionary_file, predictions_file = "dictionary.txt", "predictions.hdf5"
 
-    dictionary = [x.strip() for x in open(dictionary_file).readlines()]
-    dictionary_ = list(sorted([x.lower() for x in dictionary if len(x) == 5]))
-    game = GameState(dictionary_)
+    game = GameState(dictionary_file)
     if compute_predictions:
         with timing_context("precompute"):
             game.compute_all_predictions()
-            # with h5py.File("predictions.hdf5", "w") as f:
-            #     dset = f.create_dataset(
-            #         "data_array", data=game.predictions, compression="gzip", compression_opts=4
-            #     )
-            #     dset.attrs["num_words"] = len(dictionary)
-            #     dset.attrs["version"] = 1
+        game.save_predictions(predictions_file)
     else:
         assert not compute_predictions
-        with h5py.File(predictions_file, "r") as f:
-            game.predictions = f["data_array"][:]
-    print(len(dictionary_), game.predictions.shape[0])
-    assert len(dictionary_) * len(dictionary_) == game.predictions.shape[0]
-    with timing_context("first time"):
-        game.best_move()
+        game.load_predictions(predictions_file)
+
+    if compute_best_first_move:
+        with timing_context("first time"):
+            game.best_move()
